cf5b33ff277905ba16981ad6c200ae5b7b5109de
fehead <im.fehead@gmail.com>
2019-04-23 20:09:55 +0900

    first import.

---
+++ desktop/src/fehead/im/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fehead.im.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fehead.im.KickItUpGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KickItUpGame(), config);
+	}
+}
