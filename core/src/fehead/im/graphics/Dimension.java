a2b10fbbf41c1e71b33a8660307dbaa3337ff5af
chaehoon <chaehoon.leem@devmon.co.kr>
2019-08-20 21:20:31 +0900

    add hashCode method.

--- core/src/fehead/im/graphics/Dimension.java
+++ core/src/fehead/im/graphics/Dimension.java
@@ -1,8 +1,8 @@
 package fehead.im.graphics;
 
 public class Dimension {
-	private	final	int w;
-	private	final	int	h;
+	private	final	int w;	// width
+	private	final	int	h;	// height
 
 	private Dimension(int w, int h) {
 		this.w = w;
@@ -21,6 +21,15 @@ public class Dimension {
 			return false;
 		Dimension o = (Dimension)obj;
 		return w == o.w && h == o.h;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + h;
+		result = prime * result + w;
+		return result;
 	}
 
 }
--- core/src/fehead/im/graphics/Point.java
+++ core/src/fehead/im/graphics/Point.java
@@ -23,4 +23,12 @@ public class Point {
 		return x == o.x && y == o.y;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
 }
