8157f7db560766810e554e57dda7772dd4b41571
fehead <im.fehead@gmail.com>
2019-11-08 18:55:01 +0900

    add Double test.

--- core/src/test/StepNewTest.java
+++ core/src/test/StepNewTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.File;
 
 import org.junit.After;
@@ -24,6 +26,14 @@ public class StepNewTest {
 		StepNew	step = new StepNew();
 		step.readKSF(new File(songPath, "Beethoven Virus/Easy_1.ksf"));
 		System.out.println(step);
+	}
+	
+	@Test
+	public void doubleTest() {
+		Double	a = 1.9998;
+		System.out.println(" >>>> " + a.intValue());
+		
+		// assertThat(a.intValue() == 1).isTrue();
 	}
 
 }
